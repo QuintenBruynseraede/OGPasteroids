97752ec3516395cf066c15bddd297bdd1b3941d8
Quinten Bruynseraede <QuintenBruynseraede@users.noreply.github.com>
2017-04-15 01:52:03 +0200

    Update GameObject.java

--- src/asteroids/model/GameObject.java
+++ src/asteroids/model/GameObject.java
@@ -2,17 +2,32 @@ package asteroids.model;
 
 import be.kuleuven.cs.som.annotate.Basic;
 
-public class GameObject {
+/**
+* A class representing a GameObject, the most basic object in our game.
+*/
+public abstract class GameObject {
+	/**
+	* Variable representing the type of this GameObject. GameObject types are defined in Constants.java.
+	*/
 	private final int type;
 	
+	/**
+	* Creates a new GameObject that has a type.
+	**/
 	public GameObject(int type) {
 		this.type = type;
 	}
 	
+	/**
+	* Creates a new GameObject. If no type is specified, we assume it is a boundary.
+	**/
 	public GameObject() {
 		type = Constants.BOUNDARY;
 	}
 	
+	/**
+	* Return this GameObject's type
+	**/
 	@Basic
 	public int getType() {
 		return this.type;
