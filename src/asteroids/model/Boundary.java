59b1828b92f8bc92fe3267e0bc620e30333214df
Quinten Bruynseraede <QuintenBruynseraede@users.noreply.github.com>
2017-04-15 01:38:52 +0200

    Update Boundary.java

--- src/asteroids/model/Boundary.java
+++ src/asteroids/model/Boundary.java
@@ -1,12 +1,28 @@
 package asteroids.model;
 
+/**
+* A Class wrapping the abstract concept of a world's boundary in a usable GameObject
+*/
+
 public class Boundary extends GameObject {
+	/**
+	* Variable representing the type of this boundary. Boundary types are defined in Constants.java
+	*/
 	public final int boundaryType;
 	
+	/**
+	* Creates a new boundary of a certain type.
+	* @param the type of this boundary
+	* @post  new.getBoundaryType() == type
+	*/
 	public Boundary(int type) {
 			this.boundaryType = type;
 	}
 	
+	/**
+	* Returns the type of this boundary.
+	*/
+	@Basic
 	public int getBoundaryType() {
 		return this.boundaryType;
 	}
