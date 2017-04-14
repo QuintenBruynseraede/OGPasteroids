a170506af53f22b50bfadc2e3f0f8bffceec86be
Quinten Bruynseraede <QuintenBruynseraede@users.noreply.github.com>
2017-04-15 01:49:34 +0200

    Update Constants.java

--- src/asteroids/model/Constants.java
+++ src/asteroids/model/Constants.java
@@ -1,15 +1,27 @@
 package asteroids.model;
 
-public class Constants {
+/**
+* Class containing the game's basic constants in a readable way. Class-specific constants are defined in their respective class
+**/
+public abstract class Constants {
+	/*
+	* Boundary types
+	**/
 	public final static int LEFT = 1;
 	public final static int TOP = 2;
 	public final static int RIGHT = 3;
 	public final static int BOTTOM = 4;
 	
+	/*
+	* GameObject types
+	**/
 	public final static int BOUNDARY = 0;
 	public final static int ENTITY = 1;
 	public final static int WORLD = 2;
 	
+	/*
+	* Collision types
+	**/
 	public final static int ENTITYCOLLISION = 2;
 	public final static int BOUNDARYCOLLISION = 1;
 }
